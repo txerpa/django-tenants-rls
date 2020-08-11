33f0f6028b0a1842eca7a69a1baa27237c6bc05d
Marija Derasevic <marija.djerasevic@txerpa.com>
2020-08-11 15:22:40 +0200

    Disable rls when the last rls required field is removed from the model

--- tenant_schemas/postgresql_backend/schema.py
+++ tenant_schemas/postgresql_backend/schema.py
@@ -11,9 +11,13 @@ from tenant_schemas.utils import get_tenant_model
 class RLSDatabaseSchemaEditor(DatabaseSchemaEditor):
 
     sql_enable_rls = "ALTER TABLE %(table)s enable ROW LEVEL SECURITY"
+    sql_disable_rls = "ALTER TABLE %(table)s disable ROW LEVEL SECURITY"
     sql_force_rls = "ALTER TABLE %(table)s FORCE ROW LEVEL SECURITY"
     sql_create_policy = (
         "CREATE POLICY _po_tenant_%(table)s ON %(table)s FOR ALL USING %(policy)s"
+    )
+    sql_drop_policy = (
+        "DROP POLICY IF EXISTS _po_tenant_%(table)s ON %(table)s"
     )
     sql_alter_column_defaul_tenant = "ALTER TABLE ONLY %(table)s ALTER COLUMN tenant_id SET DEFAULT current_setting('txerpa.tenant');"
 
@@ -31,6 +35,16 @@ class RLSDatabaseSchemaEditor(DatabaseSchemaEditor):
         # enable RLS on table and create policy
         self._set_tenant_rls(enable_rls, model)
 
+    def remove_field(self, model, field):
+        is_left_rls_required_field = any(local_field.rls_required for local_field in model._meta.local_fields
+                                         if hasattr(local_field, 'rls_required') and local_field.name != field.name)
+        is_rls_required_for_field = field.rls_required if hasattr(field, 'rls_required') else False
+        disable_rls = is_rls_required_for_field and not is_left_rls_required_field
+
+        super().remove_field(model, field)
+        # disable RLS on table and delete policy
+        self._unset_tenant_rls(disable_rls, model)
+
     def _set_tenant_rls(self, enable_rls, model):
         if enable_rls:
             self.execute(self.sql_enable_rls % {"table": self.quote_name(model._meta.db_table)})
@@ -40,3 +54,8 @@ class RLSDatabaseSchemaEditor(DatabaseSchemaEditor):
                 "policy": "(tenant_id = current_setting('txerpa.tenant')) with check (tenant_id = current_setting('txerpa.tenant'))"
             })
             self.execute(self.sql_alter_column_defaul_tenant % {"table": self.quote_name(model._meta.db_table)})
+
+    def _unset_tenant_rls(self, disable_rls, model):
+        if disable_rls:
+            self.execute(self.sql_disable_rls % {"table": self.quote_name(model._meta.db_table)})
+            self.execute(self.sql_drop_policy % {"table": model._meta.db_table})
