2c587946dbad0f31be13fa7dfee7bf0c5df7ff59
biel <biel.massot@txerpa.com>
2020-12-11 10:29:15 +0100

    Changed strategy for serializers, use HiddedField instead of remove tenant field

--- tenant_schemas/contrib/drf/serializers.py
+++ tenant_schemas/contrib/drf/serializers.py
@@ -1,25 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from tenant_schemas.models import MultitenantMixin
+from tenant_schemas.models import get_tenant, MultitenantMixin
 from .utils import is_bad_tenant_field_config
 
 
-class RLSModelSerializer(ModelSerializer):
+class RLSModelSerializer(serializers.ModelSerializer):
     """
-    This class remove tenant field form serializer field's when '__all__' is specified as fields value.
-    If you want this field(tenant) in the serializer, you must specify in "fields" definition:
-       fields = ('field1', 'field2', 'tenant', ...)
+    This class add 'tenant' as HiddenField when '__all__' is specified as Meta.fields value or when 'tenant' is not
+    included in Meta.exclude.
+    Hidden fields are omitted on reads, but are considered on writes in order to check unique validators.
 
-    This is because if this field is not excluded, a select is made for each object of the result, to obtain
-    the value of the client model.
+    This is because if this field has other type, a select is made for each object of the result, to obtain the value
+    of the client model. If tenant field is omitted on write, unique_validators are not executed, and in the case of
+    duplicated values a 5xx error is given instead of 400 error.
     """
 
-    def get_field_names(self, *args, **kwargs):
-        fields = super().get_field_names(*args, **kwargs)
+    def __init__(self, instance=None, *args, **kwargs):
+        super().__init__(instance, *args, **kwargs)
 
         model = getattr(self.Meta, 'model', None)
         if issubclass(model, MultitenantMixin):
-            if is_bad_tenant_field_config(self) and 'tenant' in fields:
-                fields.remove('tenant')
+            if is_bad_tenant_field_config(self):
+                self.fields['tenant'] = serializers.HiddenField(default=get_tenant)
 
-        return fields
+
