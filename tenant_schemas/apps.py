b0a71b748becbae81e1c10b2c3f020bdd6f27eee
biel <biel.massot@txerpa.com>
2020-12-16 09:38:27 +0100

    Fix drf model serializer

--- tenant_schemas/apps.py
+++ tenant_schemas/apps.py
@@ -89,14 +89,18 @@ def best_practice(app_configs, **kwargs):
 @register('rest_framework.serializers')
 def check_serializers(app_configs, **kwargs):
     import inspect
-    from rest_framework.serializers import ModelSerializer
+    from rest_framework.serializers import ModelSerializer as drf_ModelSerializer, HyperlinkedModelSerializer
+    from rest_framework_json_api.serializers import ModelSerializer as json_api_ModelSerializer
     from .models import MultitenantMixin
     from .contrib.drf.serializers import RLSModelSerializer
 
     import config.urls  # noqa, force import of all serializers.
 
-    for serializer in ModelSerializer.__subclasses__():
-
+    for serializer in (
+        drf_ModelSerializer.__subclasses__() +
+        json_api_ModelSerializer.__subclasses__() +
+        HyperlinkedModelSerializer.__subclasses__()
+    ):
         # Skip third-party apps and serializers that have already RLSModelSerializer as base class.
         path = inspect.getfile(serializer)
         if path.find('site-packages') > -1 or issubclass(serializer, RLSModelSerializer):
